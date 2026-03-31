1a1eac7a977ce8f2cf21c2aeef206903d46dd663
Aisac3 <aisacjose4484@gmail.com>
2026-03-31 11:21:44 +0530

    Initial terrafprm setup

---
+++ code/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "staging-my-terraform-bucket-aisac" 
+    key          = "terraform/state/main/terraform.tfstate"
+    region       = "us-east-1" 
+    use_lockfile = true        
+    encrypt      = true
+  }
+}
